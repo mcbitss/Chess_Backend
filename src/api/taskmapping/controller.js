eb1e55b6961f965121ca3f0137e660e1cfd505fe
AntonyRajG <antonyrajg1995@gmail.com>
2019-02-15 13:47:29 +0530

    taskmapping updated

--- src/api/taskmapping/controller.js
+++ src/api/taskmapping/controller.js
@@ -11,13 +11,12 @@ export const createTaskMapped = (req, res, next) => {
 }
 
 export const createTaskMappedByUser = (req, res, next) => {
-  console.log('antony', 'checking', req.body, req.params)
   const user = req.params.userId ? req.params.userId : req.body.username;
   TaskMapping.findOne({ "username": user, 'taskStatus': 'Assigned'}).exec((err, resp) => {
     if (err) {} 
     else {
       if(resp === null){
-        TaskMapping.find({ "username": user}).populate('username').sort({'sequencenumber': -1}).limit(2).populate('task').exec((err, result) => {
+        TaskMapping.find({ "username": user}).sort({'sequencenumber': -1}).limit(2).populate('task').exec((err, result) => {
           if (err) {} 
           else {
             res.send({ error: false, message: 'fetch success', result: result });
@@ -27,7 +26,7 @@ export const createTaskMappedByUser = (req, res, next) => {
         const currentSequence = resp.sequencenumber;
         const lte = currentSequence === 1 && 5 || currentSequence === 2 && 5 ||  currentSequence + 2;
         const gte = currentSequence - 2;
-        TaskMapping.find({ "username": user, 'sequencenumber': { $lte: lte,  $gte: gte, }}).populate('username').populate('task').exec((err, result) => {
+        TaskMapping.find({ "username": user, 'sequencenumber': { $lte: lte,  $gte: gte, }}).populate('task').exec((err, result) => {
           if (err) {} 
           else {
             res.send({ error: false, message: 'fetch success', result: result });
@@ -36,6 +35,16 @@ export const createTaskMappedByUser = (req, res, next) => {
       }
     }
   });
+}
+
+export const showCompletedTasksMapped = (req, res, next) => {
+  TaskMapping.find({ "username": req.params.userId, taskStatus:'Completed'}).sort({'sequencenumber': -1}).populate('task').exec((err, resp) => {
+  if (err) {
+
+  } else {
+    res.send({ error: false, message: 'fetch success', result: resp });
+  }
+});
 }
 
 export const showTasksMapped = (req, res, next) => {
@@ -56,10 +65,8 @@ export const update = (req, res, next) => {
     } else {
       TaskMapping.findOneAndUpdate({'username': body.username, 'sequencenumber':body.sequencenumber + 1}, {taskStatus:'Assigned'}, {upsert: false, new: true}, (err, result) => {
         if (err) {
-          // res.send(err);
         } else {
           next()
-          // console.log('antony', 'hello')
         }
       });  
     }
--- src/api/taskmapping/index.js
+++ src/api/taskmapping/index.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { createTaskMapped, showTasksMapped, createTaskMappedByUser,update } from './controller';
+import { createTaskMapped, showTasksMapped, createTaskMappedByUser, showCompletedTasksMapped, update } from './controller';
 
 const router = new Router();
 
 router.post('/', createTaskMapped);
 router.get('/', showTasksMapped);
 router.get('/createTaskMappedByUser/:userId', createTaskMappedByUser);
+router.get('/:userId', showCompletedTasksMapped);
 router.put('/', update, createTaskMappedByUser)
 
 export default router;
