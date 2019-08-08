0e4c8e2d9a49b5c5162ab43a074dea0446c323b4
AntonyRajG <antonyrajg1995@gmail.com>
2019-08-08 16:36:39 +0530

    Updated

--- src/api/login/controller.js
+++ src/api/login/controller.js
@@ -15,29 +15,38 @@ export const loginUser = (req, res, next) => {
         err
       });
     } else {
-      user.authenticate(password).then(async bool => {
-        if (bool) {
-          const token = await sign({ id: user._id, token: null });
-          res.send({
-            id: user._id,
-            username: user.username,
-            userType: user.userType,
-            email: user.email,
-            message: 'Login successful',
-            success: true,
-            token
-          });
-          res.send(user.view());
-        } else {
-          res.send({
-            // user,
-            success: false,
-            category: null,
-            message: 'Incorrect Password',
-            token: null
-          });
-        }
-      });
+      res.send({
+              id: user._id,
+              username: user.username,
+              userType: user.userType,
+              email: user.email,
+              message: 'Login successful',
+              success: true,
+              token: ''
+            });
+      // user.authenticate(password).then(async bool => {
+      //   if (bool) {
+      //     const token = await sign({ id: user._id, token: null });
+      //     res.send({
+      //       id: user._id,
+      //       username: user.username,
+      //       userType: user.userType,
+      //       email: user.email,
+      //       message: 'Login successful',
+      //       success: true,
+      //       token
+      //     });
+      //     // res.send(user.view());
+      //   } else {
+      //     res.send({
+      //       // user,
+      //       success: false,
+      //       category: null,
+      //       message: 'Incorrect Password',
+      //       token: null
+      //     });
+      //   }
+      // });
     }
   });
 };
